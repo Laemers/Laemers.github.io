05b8176cd7b09d79ee914f6e340db9d72ab74f65
Kevin Laemers <18590570+PloxKevin@users.noreply.github.com>
2020-06-02 22:07:09 +0200

    Added jQuery

---
+++ scripts/client.js
@@ -0,0 +1,67 @@
+_cv = [];
+
+moduLoad("physics")
+moduLoad("draw")
+moduLoad("celestials")
+
+_g = { players: [] , worldScale: 60e15 };
+
+_cv.setupAll = function() {
+	$("canvas").each(function(ind) {
+		_cv[ind] = $(this)[0].getContext('2d');
+	});
+	return $("canvas").length;
+}
+
+moduLoad.ready = function() {
+	
+	_cv.setupAll();
+	new celestials("sun");
+	_g.players[0] = new celestials();
+	new celestials("jup");
+	new celestials("mar");
+	
+	$("canvas").click(function(e) {
+		var o = $(this).offset();
+		_g.players[0].pos[0] = e.pageX - o.left;
+		_g.players[0].pos[1] = e.pageY - o.top;
+	});
+	
+	setInterval(function() { step(); },1000/30);//step();
+	
+}
+
+step = function() {
+	
+	_cv[0].fillStyle = "rgb(0,0,0)";
+	_cv[1].fillStyle = "rgba(256,256,256,.2)";
+	_cv[0].fillRect(0,0,650,650);
+	
+	var l = celestials.list;
+	
+	for (var i = 0; i < l.length; i++) {
+		for (var x = 0; x < l.length; x++) {
+			if (i != x) {
+				l[i].forces.push(physics.gravity(l[i], l[x]));
+			}
+		}
+	}
+	
+	for (var i = 0; i < l.length; i++) {
+		l[i].forces.push(l[i].vel);
+		l[i].vel = physics.addvectors(l[i].forces);
+	}
+	
+	for (var i = 0; i < l.length; i++) {
+		l[i].pos[0] += Math.cos(l[i].vel[1])*l[i].vel[0]/l[i].mass/_g.worldScale;
+		l[i].pos[1] += Math.sin(l[i].vel[1])*l[i].vel[0]/l[i].mass/_g.worldScale;
+	}	
+	
+	for (var i = 0; i < l.length; i++) {
+		draw.planet(l[i]);
+		l[i].forces = [];
+		
+		_cv[1].fillRect(l[i].pos[0],l[i].pos[1],1,1);
+	}
+	
+}
